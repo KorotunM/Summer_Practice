8582a52ad17255f0aa42f621f237494b49fa5ae0
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-29 00:15:00 +0300

    aaa

--- code/Index/EmployeeIndex.php
+++ code/Index/EmployeeIndex.php
@@ -93,7 +93,7 @@ else {
     setcookie('position_error', '', 100000);
     setcookie('department_error', '', 100000);
   }
-  print(!empty($_COOKIE['id_value']));
+  print($_COOKIE['id_value']);
   if (!empty($_COOKIE['id_value'])) {
     $department_id = $_POST['department'];
     try {
