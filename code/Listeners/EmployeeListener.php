486015473debcabdf77cfe5c5ff83ca33d9cc306
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-29 23:41:08 +0300

    ашч

--- code/Listeners/EmployeeListener.php
+++ code/Listeners/EmployeeListener.php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_COOKIE['id_value'])) {
     include('../password.php');
     if (isset($_POST['change'])) {
         setcookie('id_value', '', time() - 3600);
-        setcookie('id_value', $_POST['employee_id'], time() + 24 * 60 * 60, '/');
+        setcookie('id_value', $_POST['employee_id'], time() + 24 * 60 * 60);
         setcookie('departmentid_value',$_POST['department_id'], time() + 24 * 60 * 60);
         setcookie('fio_value_' . $_COOKIE['id_value'], $_POST['fio'], time() + 12 * 30 * 24 * 60 * 60);
         setcookie('tel_value',$_POST['tel'], time() + 24 * 60 * 60);
