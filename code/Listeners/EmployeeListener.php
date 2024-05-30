9a89a644f2041136601d75e44357a6353860543f
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-30 14:23:21 +0300

    sdvdsf

--- code/Listeners/EmployeeListener.php
+++ code/Listeners/EmployeeListener.php
@@ -3,13 +3,16 @@ header('Content-Type: text/html; charset=UTF-8');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include('../password.php');
     if (isset($_POST['change'])) {
-        setcookie('id_value', $_POST['employee_id'], time() + 24 * 60 * 60, '/');
-        setcookie('department_value',$_POST['department_name'], time() + 24 * 60 * 60, '/');
-        setcookie('fio_value',$_POST['fio'], time() + 24 * 60 * 60, '/');
-        setcookie('tel_value',$_POST['tel'], time() + 24 * 60 * 60, '/');
-        setcookie('email_value',$_POST['email'], time() + 24 * 60 * 60, '/');
-        setcookie('position_value',$_POST['position'], time() + 24 * 60 * 60, '/');
-        header('Location: ../Index/EmployeeIndex.php');
+      setcookie('id_value', $_POST['employee_id'], time() + 24 * 60 * 60, '/');
+      setcookie('department_value', $_POST['department_name'], time() + 24 * 60 * 60, '/');
+      $stmt = $db->prepare("SELECT fio, tel, email, position FROM employee WHERE employee_id = ?");
+      $stmt->execute([$_POST['employee_id']]);
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      setcookie('fio_value', $result[0]['fio'], time() + 24 * 60 * 60, '/');
+      setcookie('tel_value', $result[0]['tel'], time() + 24 * 60 * 60, '/');
+      setcookie('email_value', $result[0]['email'], time() + 24 * 60 * 60, '/');
+      setcookie('position_value', $result[0]['position'], time() + 24 * 60 * 60, '/');
+      header('Location: ../Index/EmployeeIndex.php');
     }
     elseif (isset($_POST['delete'])) {
         try {
