b9ca70f83c377c724fcfd11475804a6c7f620ec2
KorotunM <144013223+KorotunM@users.noreply.github.com>
2024-05-31 00:50:02 +0300

    34е

--- code/Index/EmployeeIndex.php
+++ code/Index/EmployeeIndex.php
@@ -68,9 +68,10 @@ else {
  }
  setcookie('email_value', $_POST['email'], time() + 12 * 30 * 24 * 60 * 60);
 
- if (empty ($_POST['position']) || !preg_match('/^[a-zA-Zа-яА-Я]+$/u', $_POST['position'])) {
-    setcookie('position_error','1',time() + 24 * 60 * 60);
-    $errors = TRUE;
+ $pattern = '/^[\p{Cyrillic}A-Za-z]+$/u';
+ if (empty($_POST['position']) || !preg_match($pattern, $_POST['position'])) {
+     setcookie('position_error', '1', time() + 24 * 60 * 60);
+     $errors = TRUE;
  }
  setcookie('position_value', $_POST['position'], time() + 12 * 30 * 24 * 60 * 60);
 
